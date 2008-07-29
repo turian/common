1135d0bed0f3ffefbba64188d77ba8f5e67d152c
Yoshua Bengio <bengioy@iro.umontreal.ca>
2008-07-29 09:40:58 -0400

    copied theanify and Graph from denoising_aa

--- autoname.py
+++ autoname.py
@@ -1,3 +1,48 @@
+import sys
+from theano import gof
+
+
+class Graph:
+    def __init__(self, table):
+        for name, r in table.iteritems():
+            if isinstance(r, gof.Result):
+                if not r.name:
+                    r.name = name
+            setattr(self, name, r)
+
+
+def theanify(function):
+    """ 
+    Decorator that puts all the locals of the function to which it is
+    applied in a Graph (that sets the name fields of the Results to
+    the name of the local variables which contain them)
+
+    Returns the Graph object.
+    """
+
+    def exec_probe(function, args, kwargs):
+        func_locals = {}
+        n = []
+        def probe_func(frame, event, arg):            
+            if event == 'call':
+                if n:
+                    return None
+                else:
+                    n.append(None)
+                    return probe_func
+            if event == 'return':
+                locals = frame.f_locals
+                func_locals.update(locals)
+                sys.settrace(None)
+            return probe_func
+        sys.settrace(probe_func)
+        function(*args, **kwargs)
+        return Graph(func_locals)
+
+    def newf(*args, **kwargs):
+        return exec_probe(function, args, kwargs)
+
+    return newf
 
 
 class AutoName(object):
