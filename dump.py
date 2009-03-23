bf7f9d83facbf00a1e6e626422c3d7c4005f6fcc
Joseph Turian <turian@iro.umontreal.ca>
2009-03-23 14:27:30 -0400

    Added hyperparameters.py

--- dump.py
+++ dump.py
@@ -4,10 +4,41 @@ Dump job options and parameters to RUNDIR-hash/parameters.txt
 @todo: Allow user to choose dump file.
 """
 
+import myyaml
+
+def create_canonical_directory(*vars):
+    """
+    For these variables, create a canonical directory to hold this run's output.
+    Write the module values to 'LOGS.NOBACKUP/run-HASH/jobparameters.yaml'
+    @note: Directory is a hash for right now.
+    @todo: Make the directory name as human-readable as possible, maybe
+    with optional human-readable parameters in the name.
+    """
+    import sys, os, os.path, hashlib
+    s = myyaml.dump(vars)
+    d = "LOGS.NOBACKUP/run-%s" % hashlib.sha224(s).hexdigest()
+    sys.stderr.write("Opening canonical directory: %s\n" % d)
+    if not os.path.exists(d): os.mkdir(d)
+    assert os.path.exists(d)
+    params = os.path.join(d, "jobparameters.yaml")
+    if not os.path.exists(params): open(params, "wt").write(s)
+    assert open(params, "rt").read() == s
+    return d
+
+def load_canonical_directory(d):
+    """
+    Open this run directory, and return a YAML string containing those parameters.
+    """
+    import os.path
+    assert os.path.exists(d)
+    params = os.path.join(d, "jobparameters.yaml")
+    assert os.path.exists(params)
+    return open(params, "rt").read()
+
 from types import *
 def vars(module, regex="^[A-Za-z]"):
     """
-    Dump the variables in a module.
+    Get the variables in a module, and insert them as elements in a dict.
     I use this to dump the hyperparameters, for logging.
     @todo: Use inspect module?
 
@@ -47,55 +78,33 @@ def vars(module, regex="^[A-Za-z]"):
 [17:49] <arkanes> twas: the way you're doing it is pretty much doomed to be a PITA
     """
     import re
-    s = ""
+    d = {}
+#    s = ""
 #    for name, value in vars(module).iteritems():
 #        s += "%s %s %r\n" % (("%s.%s" % (module.__name__, name)).ljust(50), "=", value)
     varre = re.compile(regex)
     for var in module.__dict__:
         value = module.__dict__[var]
         if type(value) in [FunctionType, ModuleType]: continue
-        if varre.match(var[0]): s += "%s %s %r\n" % (("%s.%s" % (module.__name__, var)).ljust(50), "=", value)
-    if s != "":
-        s = "import %s\n" % module.__name__ + s
-    return s
+        if varre.match(var[0]):
+            d[var] = value
+#           s += "%s %s %r\n" % (("%s.%s" % (module.__name__, var)).ljust(50), "=", value)
+#    if s != "":
+#        s = "import %s\n" % module.__name__ + s
+#    return s
+    return d
 
 def vars_seq(modules, regex="^[A-Za-z]"):
     """
-    Dump the variables in several modules.
+    Dump the variables in several modules into a dict.
     I use this to dump the hyperparameters, for logging.
     @todo: Use inspect module?
     """
-    s = ""
+#    s = ""
+#    for m in modules:
+#        s += vars(m, regex)
+#    return s
+    d = {}
     for m in modules:
-        s += vars(m, regex)
-    return s
-
-def create_canonical_directory(modules):
-    """
-    For these modules, create a canonical directory to hold this run's output.
-    Write the module values to 'LOGS.NOBACKUP/run-HASH/parameters.txt'
-    @note: Directory is a hash for right now.
-    @todo: Make the directory name as human-readable as possible, maybe
-    with optional human-readable parameters in the name.
-    """
-    import sys, os, os.path, hashlib
-    s = vars_seq(modules)
-    d = "LOGS.NOBACKUP/run-%s" % hashlib.sha224(s).hexdigest()
-    sys.stderr.write("Opening canonical directory: %s\n" % d)
-    if not os.path.exists(d): os.mkdir(d)
-    assert os.path.exists(d)
-    params = os.path.join(d, "jobparameters.py")
-    if not os.path.exists(params): open(params, "wt").write(s)
-    assert open(params, "rt").read() == s
+        d[module.__name__] = vars(m, regex)
     return d
-
-def load_canonical_directory(d):
-    """
-    Open this run directory, and return a string containing those parameters.
-    @note: This string can be exec()'ed, although this may be a security hole.
-    """
-    import os.path
-    assert os.path.exists(d)
-    params = os.path.join(d, "jobparameters.py")
-    assert os.path.exists(params)
-    return open(params, "rt").read()
---
+++ hyperparameters.py
@@ -0,0 +1,52 @@
+"""
+Module for reading hyperparameters.
+
+@todo: Maybe ensure that script locations are executable.
+"""
+
+_HYPERPARAMETERS = {}
+
+def read(suffix=None):
+    """
+    suffix is the name of these hyperparameters, e.g. "nlpreprocess"
+    for the "hyperparameters.nlpreprocess.yaml" file.
+    """
+    if suffix in _HYPERPARAMETERS: return _HYPERPARAMETERS[suffix]
+
+    import common.file, os.path, yaml, sys
+    if suffix: f = "hyperparameters.%s.yaml" % suffix
+    else: f = "hyperparameters.yaml" % suffix
+    __file = common.file.ascend_find(f)
+    h = yaml.load(open(__file).read())
+    if os.path.realpath(__file) != __file:
+        print >> sys.stderr, "NOTE: os.path.realpath(%s) = %s" % (__file, os.path.realpath(__file))
+    h["basedir"] = os.path.dirname(os.path.realpath(__file))
+    
+    # Prepend basedir to all relative locations
+    if "locations" not in h: h["locations"] = {}
+    if "relative locations" in h:
+        for l in h["relative locations"]:
+            h["locations"][l] = os.path.join(h["basedir"], h["relative locations"][l])
+        # Now delete relative locations, since we only care about the absolute ones
+        del h["relative locations"]
+    
+    # Expand ~ and check if locations exist
+    for l in h["locations"]:
+        h["locations"][l] = os.path.expanduser(h["locations"][l])
+        loc = h["locations"][l]
+        if not os.path.exists(loc):
+            print >> sys.stderr, "WARNING . Location for %s does not exist: %s" % (`l`, loc)
+    
+    # Prepend work dir to all work locations
+    if "work locations" in h:
+        for l in h["work locations"]:
+            h["locations"][l] = os.path.join(h["locations"]["work dir"], h["work locations"][l])
+        # Now delete relative locations, since we only care about the absolute ones
+        del h["work locations"]
+    
+    print >> sys.stderr, "# BEGIN %s h" % suffix
+    print >> sys.stderr, yaml.dump({suffix: h}),
+    print >> sys.stderr, "# END %s h" % suffix
+
+    _HYPERPARAMETERS[suffix] = h
+    return _HYPERPARAMETERS[suffix]
