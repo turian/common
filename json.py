ad54d3dd341b1129e1303339ea7f44246add891c
Joseph Turian <turian@iro.umontreal.ca>
2009-03-24 21:22:27 -0400

    Use simplejson, not cjson (cjson is buggy)
    --HG--
    rename : json.py => json.cjson.py.old

--- json.py
+++ json.py
@@ -1,46 +1,22 @@
 """
 JSON convenience routines.
-
-We use python-cjson because it is far faster than simplejson on certain installs:
-    http://blog.metaoptimize.com/2009/03/22/fast-deserialization-in-python/
-
-Note that cjson 1.0.5 is buggy:
-    http://blog.extracheese.org/2007/07/when-json-isnt-json.html
-    http://www.vazor.com/cjson.html
 """
 
-import cjson
-if cjson.__version__ != "1.0.6":
-    import sys
-    print >> sys.stderr, "WARNING: cjson < 1.0.6 is buggy (your version=%s)" % cjson.__version__
-    print >> sys.stderr, "    http://blog.extracheese.org/2007/07/when-json-isnt-json.html"
-    print >> sys.stderr, "    http://www.vazor.com/cjson.html"
+import simplejson
 
-encode = cjson.encode
-decode = cjson.decode
-
-loads = decode
-dumps = encode
+loads = simplejson.loads
+dumps = simplejson.dumps
+load = simplejson.load
+dump = simplejson.dump
 
 from common.file import myopen
-def load(file):
-    """
-    Load JSON from a stream.
-    """
-    return decode(file.read())
-def dump(file, object):
-    """
-    Dump JSON to a stream.
-    """
-    return file.write(encode(filename, object))
-
 def loadfile(filename):
     """
     Load JSON from a filename.
     """
     return load(myopen(filename))
-def dumpfile(filename, object):
+def dumpfile(object, filename):
     """
     Dump JSON to a filename.
     """
-    return dump(myopen(filename, "wb"))
+    return dump(object, myopen(filename, "wb"))
--- maths.py
+++ maths.py
@@ -4,3 +4,6 @@ def logistic(x):
     @todo: WRITEME
     """
     return 1./(1+math.exp(-x))
+
+def round(f):
+    return int(f + 0.5)
