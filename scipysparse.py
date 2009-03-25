aae952fde337118393abf8a16c589d601f00948d
Joseph Turian <turian@iro.umontreal.ca>
2009-03-24 23:15:53 -0400

    Added scipy.sparse

---
+++ scipysparse.py
@@ -0,0 +1,6 @@
+
+import scipy
+if scipy.__version__ != "0.7.0":
+    print >> sys.stderr, "WARNING! scipy < 0.7 has BUGGY sparse implementation. Your version =", scipy.__version__
+from scipy.sparse import *
+
