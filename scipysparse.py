13105152fe27bf06808bb10c00245794dce38f68
Joseph Turian <turian@gmail.com>
2009-05-08 17:47:17 -0400

    Small bugfix: import sys

--- scipysparse.py
+++ scipysparse.py
@@ -1,6 +1,7 @@
 
 import scipy
 if scipy.__version__ != "0.7.0":
+    import sys
     print >> sys.stderr, "WARNING! scipy < 0.7 has BUGGY sparse implementation. Your version =", scipy.__version__
 from scipy.sparse import *
 
