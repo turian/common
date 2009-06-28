56e4dd885b2c0df6822cc77ef24b963e7fc0cf58
Joseph Turian <turian@gmail.com>
2009-06-27 21:19:41 -0400

    Made common.hashdb.JSONHDB

--- hashdb.py
+++ hashdb.py
@@ -7,13 +7,23 @@ from pytc import HDB, HDBOWRITER, HDBOCREAT, HDBTBZIP, HDBOREADER
 import os.path
 import common.json
 
+class JSONHDB(HDB):
+    def __getitem__(self, key):
+        return common.json.loads(HDB.__getitem__(self, key))
+    def __setitem__(self, key, value):
+        return HDB.__setitem__(self, key, common.json.dumps(value))
+    def get(self, key):
+        return common.json.loads(HDB.get(self, key))
+    def put(self, key, value):
+        return HDB.put(self, key, common.json.dumps(value))
+
 def create(filename):
     """
     Create a hashdb with this filename.
     Abort if this file already exists.
     """
     assert not os.path.exists(filename)
-    hdb = HDB()
+    hdb = JSONHDB()
     #     bnum - the number of elements of the bucket array. If it is
     #     not more than 0, the default value is specified. The default value
     #     is 131071. Suggested size of the bucket array is about from 0.5
@@ -34,13 +44,12 @@ def read(filename):
     A generator that iterates over the (key, value) pairs in the TCH in filename.
     We assume that each value is a JSON object.
     """
-    hdb = HDB()
+    hdb = JSONHDB()
     hdb.open(filename, HDBOREADER)
     
     # traverse records
     hdb.iterinit()
     for key in hdb.keys():
         value = hdb.get(key)
-        value = common.json.loads(value)
         yield (key, value)
     hdb.close()
