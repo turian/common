89e317d774017c862f6736fb2978e9490d7a266c
Joseph Turian <turian@gmail.com>
2009-06-27 20:58:08 -0400

    Added common.hashdb

---
+++ hashdb.py
@@ -0,0 +1,46 @@
+"""
+Wrapper for hash database functions.
+Uses Tokyo Cabinet hashdb.
+"""
+
+from pytc import HDB, HDBOWRITER, HDBOCREAT, HDBTBZIP, HDBOREADER
+import os.path
+import common.json
+
+def create(filename):
+    """
+    Create a hashdb with this filename.
+    Abort if this file already exists.
+    """
+    assert not os.path.exists(filename)
+    hdb = HDB()
+    #     bnum - the number of elements of the bucket array. If it is
+    #     not more than 0, the default value is specified. The default value
+    #     is 131071. Suggested size of the bucket array is about from 0.5
+    #     to 4 times of the number of all records to be stored.
+    #     apow - the size of record alignment by power of 2. If it is
+    #       negative, the default value is specified. The default value is
+    #       4 standing for 2^4=16.
+    #     fpow - the maximum number of elements of the free block pool
+    #       by power of 2. If it is negative, the default value is
+    #       specified. The default value is 10 standing for 2^10=1024.
+    hdb.tune(bnum=131071, apow=4, fpow=10, opts=HDBTBZIP)
+    #hdb.tune(bnum=131071, apow=4, fpow=10, opts=0)
+    hdb.open(filename, HDBOCREAT | HDBOWRITER)
+    return hdb
+
+def read(filename):
+    """
+    A generator that iterates over the (key, value) pairs in the TCH in filename.
+    We assume that each value is a JSON object.
+    """
+    hdb = HDB()
+    hdb.open(filename, HDBOREADER)
+    
+    # traverse records
+    hdb.iterinit()
+    for key in hdb.keys():
+        value = hdb.get(key)
+        value = common.json.loads(value)
+        yield (key, value)
+    hdb.close()
