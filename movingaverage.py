a2e6989cb371eb09333e0120b765dbd3be83a842
Joseph Turian <turian@gmail.com>
2009-07-14 02:54:12 -0400

    Added moving average.

---
+++ movingaverage.py
@@ -0,0 +1,44 @@
+"""
+Moving average.
+
+Yoshua Bengio:
+
+    My preferred style of moving average is the following. Let's say you
+    have a series x_t and you want to estimate the mean m of previous
+    (recent) x's:
+
+    m <-- m - (2/t) (m - x_t)
+
+    Note that with (1/t) learning rate instead of (2/t) you get the exact
+    historical average. With a larger learning rate (like 2/t) you give
+    a bit more importance to recent stuff, which makes sense if x's are
+    non-stationary (very likely here [in the setting of computing the
+    moving average of the training error]). With a constant learning rate
+    (independent of t) you get an exponential moving average.
+
+    You can estimate a running average of the gradient variance by running
+    averages of the mean gradient and of the
+    square of the difference to the moving mean.
+
+"""
+
+class MovingAverage:
+    """
+    .mean and .variance expose the moving average estimates.
+    """
+    def __init__(self):
+        self.mean = 0.
+        self.variance = 0
+        self.cnt = 0
+    def add(self, v):
+        """
+        Add value v to the moving average.
+        """
+        self.cnt += 1
+        self.mean = self.mean - (2. / self.cnt) * (self.mean - v)
+        # I believe I should compute self.variance AFTER updating the moving average, because
+        # the estimate of the mean is better.
+        # Yoshua concurs.
+        this_variance = (v - self.mean) * (v - self.mean)
+        self.variance = self.variance - (2. / self.cnt) * (self.variance - this_variance)
+
