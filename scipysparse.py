205c589a8ba0533e5743f1f343167c18a01c57dd
Joseph Turian <turian@gmail.com>
2009-07-14 05:27:04 -0400

    Made common.scipysparse.threshold much faster and more generic

--- scipysparse.py
+++ scipysparse.py
@@ -9,26 +9,24 @@ import sys
 from common.stats import stats
 import common.str
 
-def threshold_csr(x, min):
+def threshold(x, min):
     """
-    Take x, convert it to a lil_matrix, remove all data less than min, and convert to a CSR matrix.
+    Take x and remove all data less than min.
     """
     removed = 0
     tot = 0
-    x = scipy.sparse.lil_matrix(x)
-    print >> sys.stderr, "Applying threshold %.3f to %d rows\n\tCurrently %d nonzeros (%.3f nonzeros per row)..." % (min, x.shape[0], x.nnz, 1.*x.nnz/x.shape[0])
+    print >> sys.stderr, "Applying threshold %.3f to %d rows\nCurrently %d nonzeros (%.3f nonzeros per row)..." % (min, x.shape[0], x.nnz, 1.*x.nnz/x.shape[0])
     print >> sys.stderr, stats()
-    (row, col) = x.nonzero()
-    for i, (r,c) in enumerate(zip(row.tolist(), col.tolist())):
-        if x[r,c] < min:
-            x[r,c] = 0
+    for i in range(len(x.data)):
+        if x.data[i] < min:
+            x.data[i] = 0
             removed += 1
         tot += 1
-        if i % 100000 == 0:
-            print >> sys.stderr, "\t%s nonzeros done" % common.str.percent(i, len(row))
+        if (i+1) % 1000000 == 0:
+            print >> sys.stderr, "\t%s nonzeros done" % common.str.percent(i+1, len(x.data))
             print >> sys.stderr, stats()
-    x = scipy.sparse.csr_matrix(x)
     x.eliminate_zeros()
-    print >> sys.stderr, "..done applying threshold %.3f to %d rows\n\tCurrently %d nonzeros (%.3f nonzeros per row)..." % (min, x.shape[0], x.nnz, 1.*x.nnz/x.shape[0])
+    print >> sys.stderr, "..done applying threshold %.3f to %d rows\nCurrently %d nonzeros (%.3f nonzeros per row)..." % (min, x.shape[0], x.nnz, 1.*x.nnz/x.shape[0])
     print >> sys.stderr, "\t%s of entries were pruned\n" % common.str.percent(removed, tot)
     print >> sys.stderr, stats()
+    return x
