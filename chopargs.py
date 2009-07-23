202e5cf5ba839f957a1a5be2745f576b02deebc5
Joseph Turian <turian@gmail.com>
2009-07-22 22:32:09 -0400

    Changed contract of chop args, to make it much less shoot-yourself in the foot prone.

--- chopargs.py
+++ chopargs.py
@@ -5,14 +5,15 @@ Module for chopping args into tuples of particular lengths.
 def chopargs(args, lengths):
     """
     Chopping args into tuples of particular lengths.
+    If a tuple has length "0", return a single item.
 
-    >>> chopargs(range(7), (1,3,1,2))
-    [0, (1, 2, 3), 4, (5, 6)]
+    >>> chopargs(range(7), (0,3,1,2))
+    [0, (1, 2, 3), (4,), (5, 6)]
     """
     r = []
     idx = 0
     for l in lengths:
-        if l == 1:
+        if l == 0:
             r.append(args[idx])
         else:
             r.append(tuple(args[idx:idx+l]))
