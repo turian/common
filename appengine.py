2de00eddf7c2cd02eb4b9cb7a8fc7424354d8036
Joseph Turian <turian@gmail.com>
2009-08-02 23:26:58 -0400

    Added common.appengine

---
+++ appengine.py
@@ -0,0 +1,41 @@
+"""
+Helper functions for app engine.
+"""
+
+from google.appengine.ext import db
+import sys
+
+BATCH_SIZE = 1000
+def delete_entire_model(model):
+    """
+    Delete all entries from a model.
+    """
+    keys = model.all(keys_only=True).fetch(BATCH_SIZE)
+    start = 0
+    print >> sys.stderr, "Deleting %d-%d entities from %s..." % (start, start+len(keys), model)
+    while len(keys) > 0:
+        start += len(keys)
+        db.delete(keys)
+        keys = model.all(keys_only=True).fetch(BATCH_SIZE)
+        print >> sys.stderr, "Deleting %d-%d entities from %s..." % (start, start+len(keys), model)
+    return True
+
+
+def key_to_path(key):
+    """
+    Convert a key to a path.
+    Code from David Wilson: http://osdir.com/ml/GoogleAppEngine/2009-05/msg01182.html
+    """
+    output = []
+    while key:
+        name = key.name()
+        if name is None:
+            assert key.has_id_or_name()
+            output.append(key.id_or_name())
+        else:
+            output.append(name)
+        output.append(key.kind())
+        key = key.parent()
+
+    output.reverse()
+    return output
