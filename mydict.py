f57a5361804b64dbdb4617dad015055e54bf9f1c
Joseph Turian <turian@gmail.com>
2009-08-24 00:49:16 -0400

    Added common.mydict.threshold

--- mydict.py
+++ mydict.py
@@ -5,8 +5,19 @@ Dict helper functions.
 def sort(d, increasing=False):
     """
     Sort a dict in decreasing order.
+    @note: this function might have sideeffects
     """
     d = [(d[k], k) for k in d]
     d.sort()
     if not increasing: d.reverse()
     return d
+
+def threshold(dict, min):
+    """
+    Remove from dict all keys less than this value, and return it.
+    @note: this function might have sideeffects
+    """
+    keys = dict.keys()
+    for k in keys:
+        if dict[k] < min: del dict[k]
+    return dict
