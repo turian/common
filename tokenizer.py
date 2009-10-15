a8b9a9a931fe43971b8e24bb9b1db23137bd20cf
Joseph Turian <turian@gmail.com>
2009-10-15 15:48:03 -0400

    Updated common.tokenize for splitta SVN HEAD

--- tokenizer.py
+++ tokenizer.py
@@ -3,29 +3,22 @@
 Tokenize a text into one-sentence-per-line (a list of sentences) using splitta.
 
 NOTE:
-    * After you import common.tokenizer, you need the following line:
-        from sbd import SVM_Model
-    Otherwise, you will get the following error:
-        AttributeError: 'module' object has no attribute 'SVM_Model'
-        
-    * Unfortunately, there need to be a handful of changes made to
-    splitta to get it to work.  First, you have to rename its util.py
-    to sbd_util.py (this may not be necessary).
-    Second, you need to add parameter files_already_opened to sbd.get_data.
+    * Unfortunately, there need to be some changes made to
+    splitta to get it to work.
+    You need to add parameter files_already_opened to sbd.get_data.
 
-    * I recommend the latest release version of splitta, 1.0.2.
-    Dan Gillick, the author of splitta, says that SVN HEAD has
-    models trained on slightly more data. However, I see one or two
-    minor bugs in SVN HEAD, e.g. util.py should be named sbd_util.py or
-    you get an ImportError.
+    * I recommend the SVN HEAD version of splitta.
+    Models in 1.0.2 are stored as pickle files, which make it very
+    difficult to load the models from outside the original splitta code.
+
+    * I chdir into the splitta dir and the chdir to the original working
+    directory. This is vestigial code from my 1.02 API and might not
+    be necessary anymore.
 """
 
-SPLITTA_DIR = "/home/joseph/utils/src/splitta-1.02/"
-#SPLITTA_DIR = "/home/joseph/utils/src/splitta.svn/"
-
 import os, os.path, sys, string
-assert os.path.isdir(SPLITTA_DIR)
-sys.path.append(SPLITTA_DIR)
+assert os.path.isdir(splitta_dir)
+sys.path.append(splitta_dir)
 
 import StringIO
 
@@ -36,24 +29,25 @@ from sbd import SVM_Model, NB_Model
 
 models = {}
 
-def tokenize(text, tokenize=False, model_path="model_svm"):
-#def tokenize(text, tokenize=False, model_path="model_nb"):
-#def tokenize(text, tokenize=False, model_path="/home/joseph/utils/src/splitta.svn/model_svm"):
+def tokenize(text, tokenize=False, splitta_dir="/home/joseph/utils/src/splitta.svn/", model_path="model_svm", verbose=False):
     oldwd = os.getcwd()
-    assert os.path.isdir(SPLITTA_DIR)
-    os.chdir(SPLITTA_DIR)
+    assert os.path.isdir(splitta_dir)
+    os.chdir(splitta_dir)
 
     if not model_path.endswith('/'): model_path += '/'
     assert os.path.isdir(model_path)
 
     # Cache model so it need not be repeatedly reloaded.
     if model_path not in models: 
-        models[model_path] = sbd.load_sbd_model(model_path)
+        # Load the model, it is not cached
+        if 'svm' in model_path: svm = True
+        else: svm = False
+        models[model_path] = sbd.load_sbd_model(model_path, svm)
     model = models[model_path]
 
     test = sbd.get_data(StringIO.StringIO(text), tokenize=True, files_already_opened=True)
-    test.featurize(model)
-    model.classify(test)
+    test.featurize(model, verbose=verbose)
+    model.classify(test, verbose=verbose)
     output = StringIO.StringIO()
     test.segment(use_preds=True, tokenize=tokenize, output=output)
 
