470315afa08e8e858dec2df9acd343b874f8540e
Joseph Turian <turian@gmail.com>
2009-12-18 03:45:37 -0500

    common.pdf.readfile now returns title, author, text

--- pdf.py
+++ pdf.py
@@ -12,13 +12,15 @@ from pyPdf import PdfFileReader
 
 def readfile(filename, pdftotext="/usr/bin/pdftotext"):
     """
-    Read a PDF file and return a title, text tuple.
+    Read a PDF file and return a title, author, text tuple.
     """
     assert os.path.exists(filename)
     assert os.path.exists(pdftotext)
     text = runcmd("%s %s -" % (pdftotext, filename))
-    title = PdfFileReader(file(filename, "rb")).getDocumentInfo().title
-    return title, text
+    i = PdfFileReader(file(filename, "rb"))
+    title = i.getDocumentInfo().title
+    author = i.getDocumentInfo().author
+    return title, author, text
 
 
 if __name__ == "__main__":
