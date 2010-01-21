fb4b5601ef1baa8abcf00bb107e25d7eb6a4dd96
Joseph Turian <turian@gmail.com>
2010-01-20 19:50:36 -0500

    Added nowrap parameter to common.tidy.tidy

--- tidy.py
+++ tidy.py
@@ -11,7 +11,7 @@ TODO: Trap stderr output.
 
 from common.misc import runcmd
 
-def tidy(markup, xml=False, forceoutput=False, veryquiet=True, indent=True):
+def tidy(markup, xml=False, forceoutput=False, veryquiet=True, indent=True, nowrap=False):
     """
     If veryquiet, all errors and warnings from tidy are written to /dev/null.
     """
@@ -20,5 +20,6 @@ def tidy(markup, xml=False, forceoutput=False, veryquiet=True, indent=True):
     if veryquiet: tidyoptions += " -f /dev/null"
     if xml: tidyoptions += " -xml"
     if indent: tidyoptions += " -indent"
+    if nowrap: tidyoptions += " -w 0"
     tidymarkup = runcmd("tidy -quiet %s" % tidyoptions, input=markup.encode("utf-8"), acceptable_return_codes=[0,1])
     return tidymarkup
