d048ed45d68718d5b5fcd2b9bf524331d1b69834
Joseph Turian <turian@gmail.com>
2010-10-21 21:54:08 +0000

    keys can now be added to common.idmap.IDmap

--- idmap.py
+++ idmap.py
@@ -19,20 +19,24 @@ class IDmap:
     If allow_unknown=True (False by default), then all unknown words get mapped
     to one OOV token id.
     """
-    def __init__(self, keys, allow_unknown=False, unknown_key="*UNKNOWN*"):
+    def __init__(self, keys=[], allow_unknown=False, unknown_key="*UNKNOWN*"):
         self.unknown_key = unknown_key
         self.allow_unknown = allow_unknown
         self.map = {}
         self.reverse_map = []
         if self.allow_unknown:
-            assert self.unknown_key not in keys
-            self.map[self.unknown_key] = len(self.reverse_map)
-            self.reverse_map.append(self.unknown_key)
-            assert self.exists(self.unknown_key) and self.key(self.id(self.unknown_key)) == self.unknown_key
+            self.add(self.unknown_key)
         for key in keys:
-            self.map[key] = len(self.reverse_map)
-            self.reverse_map.append(key)
-            assert self.exists(key) and self.key(self.id(key)) == key
+            self.add(key)
+
+    def add(self, key):
+        """ Add key to map. """
+        if key != self.unknown_key:
+            assert not self.exists(key)
+        assert key not in self.map
+        self.map[key] = len(self.reverse_map)
+        self.reverse_map.append(key)
+        assert self.exists(key) and self.key(self.id(key)) == key
 
     def exists(self, key):
         """ Return True iff this key is in the map, or if self.allow_unknown is True """
